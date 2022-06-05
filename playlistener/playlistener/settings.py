8352e7e689711379a58a57cdcbdf497486efea69
Noah Kim <noahbkim@gmail.com>
2022-06-05 14:09:40 -0400

    Fix static dir

--- playlistener/playlistener/settings.py
+++ playlistener/playlistener/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
